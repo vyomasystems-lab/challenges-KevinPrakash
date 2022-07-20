a92b1957b2937a061e1fe5e76a1e667789f8f3a0
Kevin Prakash <kevinprakash30@gmail.com>
2022-07-20 15:29:22 +0000

    test error message updated, sel_bits fixed

--- level1_design1/test_mux.py
+++ level1_design1/test_mux.py
@@ -6,7 +6,7 @@ from cocotb.triggers import Timer
 @cocotb.test()
 async def test_mux(dut):
     """Test for mux2"""
-    SEL_BITS = 3
+    SEL_BITS = 5
     IN_BITS = 2
     error_message = ""
 
@@ -19,7 +19,7 @@ async def test_mux(dut):
    
             if(dut.out.value != val):
               print("Mux output is incorrect. Expected:{EXP},Obtained:{OBT} for sel:{SEL}".format(EXP=val,OBT=dut.out.value.integer,SEL=sel))
-              error_message = error_message+str(sel) + "(inp="+str(val)+", out="+str(dut.out.value.integer)+", sel="+str(sel)+"),"
+              error_message = error_message + "(inp="+str(val)+", out="+str(dut.out.value.integer)+", sel="+str(sel)+"),"
               
             exec("dut.%s.value = %d" % (("inp"+str(sel)),0))
     
