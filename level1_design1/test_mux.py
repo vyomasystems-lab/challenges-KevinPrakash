86f10e4571a0450c99c10dfb86c47c14e06c89e4
Kevin Prakash <kevinprakash30@gmail.com>
2022-07-20 15:27:42 +0000

    test with all stimulus

--- level1_design1/test_mux.py
+++ level1_design1/test_mux.py
@@ -6,5 +6,20 @@ from cocotb.triggers import Timer
 @cocotb.test()
 async def test_mux(dut):
     """Test for mux2"""
+    SEL_BITS = 5
+    IN_BITS = 2
 
+    for sel in range(0,2**SEL_BITS):
+        for val in range(0,2**IN_BITS):
+            dut.sel.value = sel
+            exec("dut.%s.value = %d" % (("inp"+str(sel)),val))
+
+            await Timer(1,units="ns")
+   
+            if(dut.out.value != val):
+              print("Mux output is incorrect. Expected:{EXP},Obtained:{OBT} for sel:{SEL}".format(EXP=val,OBT=dut.out.value.integer,SEL=sel))
+              
+            exec("dut.%s.value = %d" % (("inp"+str(sel)),0))
+            
     cocotb.log.info('##### CTB: Develop your test here ########')
+
