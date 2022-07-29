f3eea4874a2bb011dbea6e2118e1aa69364f93e6
Kevin Prakash <kevinprakash30@gmail.com>
2022-07-29 10:03:27 +0000

    Moved design to single file. Updated the test for multiplier

--- level3_design/carry_save_multiplier.v
+++ level3_design/carry_save_multiplier.v
@@ -76,3 +76,47 @@ module csmulti_fullbasecell#(parameter bitsize = 8)(factor0, factor1, product);
   end
 
 endmodule // Parameterized Carry Save Multiplier
+
+module basecell_ha(f1_i, f2_i, b_i, sum_o, c_o);
+  input f1_i, f2_i, b_i;
+  output sum_o, c_o;
+
+  wire pp;
+
+  assign pp = f1_i & f2_i;
+
+  HA adder(pp, b_i, sum_o, c_o);
+
+endmodule // Base cell with half adder
+
+module basecell_fa(f1_i, f2_i, b_i, c_i, sum_o, c_o);
+  input f1_i, f2_i, b_i, c_i;
+  output sum_o, c_o;
+
+  wire pp;
+
+  assign pp = f1_i & f2_i;
+
+  FA adder(pp, b_i, c_i, sum_o, c_o);
+
+endmodule // Base cell with full adder
+
+//Simple 1 bit full adder
+module FA(A, B, Cin, S, Cout);
+  input A, B, Cin;
+  output S, Cout;
+  wire ha_sum; //Partial Sum without carry
+
+  assign ha_sum = A ^ B;
+  assign S =  ha_sum ^ Cin; //Sum
+  assign Cout = (A & B) | (ha_sum & Cin); //Carry out
+endmodule // full Adder
+
+//Simple 1 bit half adder
+module HA(A, B, S, Cout);
+  input A, B;
+  output S, Cout;
+
+  assign S = A ^ B;
+  assign Cout = A & B;
+endmodule // half Adder
--- level3_design/test_carry_save_multiplier.py
+++ level3_design/test_carry_save_multiplier.py
@@ -5,24 +5,13 @@ from cocotb.triggers import Timer
 
 @cocotb.test()
 async def test_mux(dut):
-    """Test for mux2"""
-    SEL_BITS = 5
-    IN_BITS = 2
-    error_message = ""
-
-    for sel in range(0,2**SEL_BITS-1):
-        for val in range(0,2**IN_BITS):
-            dut.sel.value = sel
-            exec("dut.%s.value = %d" % (("inp"+str(sel)),val))
-
-            await Timer(1,units="ns")
-   
-            if(dut.out.value != val):
-              #print("Mux output is incorrect. Expected:{EXP},Obtained:{OBT} for sel:{SEL}".format(EXP=val,OBT=dut.out.value.integer,SEL=sel))
-              error_message = error_message + "(inp="+str(val)+", out="+str(dut.out.value.integer)+", sel="+str(sel)+"),"
-              
-            exec("dut.%s.value = %d" % (("inp"+str(sel)),0))
     
-    assert len(error_message) == 0, "Output not matching for following stimulus: {MSG}".format(MSG=error_message[:-1])
+    dut.factor0.value = 5
+    dut.factor1.value = 8
+
+    await Timer(1) 
+
+    print(str(dut.factor0.value.integer) +" x " + str(dut.factor1.value.integer) + " = " + str(dut.product.value.integer))
+    #assert len(error_message) == 0, "Output not matching for following stimulus: {MSG}".format(MSG=error_message[:-1])
             
     cocotb.log.info('##### CTB: Develop your test here ########')
